e29ee52dd35e8f02b245a10cc5cd817f2fc0aae3
Brandon Worl <naosyth@gmail.com>
2014-10-15 13:56:22 -0700

    Added role field and display on account page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     # the User has not yet been activated
     if @user.save
       flash[:notice] = "Your account has been created."
-      redirect_to user_url
+      redirect_to @user
     else
       flash[:notice] = "There was a problem creating you."
       render :action => :new
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141008183029) do
+ActiveRecord::Schema.define(version: 20141015203152) do
 
   create_table "user_sessions", force: true do |t|
     t.string   "session_id", null: false
@@ -24,15 +24,15 @@ ActiveRecord::Schema.define(version: 20141008183029) do
   add_index "user_sessions", ["updated_at"], name: "index_user_sessions_on_updated_at", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "name",                default: "", null: false
-    t.string   "crypted_password",                 null: false
-    t.string   "password_salt",                    null: false
-    t.string   "email",                            null: false
-    t.string   "persistence_token",                null: false
-    t.string   "single_access_token",              null: false
-    t.string   "perishable_token",                 null: false
-    t.integer  "login_count",         default: 0,  null: false
-    t.integer  "failed_login_count",  default: 0,  null: false
+    t.string   "name",                default: "",        null: false
+    t.string   "crypted_password",                        null: false
+    t.string   "password_salt",                           null: false
+    t.string   "email",                                   null: false
+    t.string   "persistence_token",                       null: false
+    t.string   "single_access_token",                     null: false
+    t.string   "perishable_token",                        null: false
+    t.integer  "login_count",         default: 0,         null: false
+    t.integer  "failed_login_count",  default: 0,         null: false
     t.datetime "last_request_at"
     t.datetime "current_login_at"
     t.datetime "last_login_at"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20141008183029) do
     t.string   "last_login_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role",                default: "student"
   end
 
 end
