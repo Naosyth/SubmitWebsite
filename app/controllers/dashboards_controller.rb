a142ee358006adce8f5b1445587c543316e04f6d
Nolan Burfield <nburf@nevada.unr.edu>
2014-10-27 22:30:22 -0700

    Revert "Got Specific Roles to load distinct pages"
    This reverts commit 706e440bc0a5a03d8b0ee0f96fa9ffd22695f966.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,14 +2,6 @@ class DashboardsController < ApplicationController
   before_filter :require_user
 
   def show
-  	@user = current_user
-    if @user.role == "admin"
-    	render :action => :admin
-    elsif @user.role == "teacher"
-    	render :action => :teacher
-    else
-    	render :action => :student
-    end
+    @user = current_user
   end
-
 end
