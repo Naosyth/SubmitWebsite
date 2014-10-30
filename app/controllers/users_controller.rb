54090de6f2dd0ffdf1079ee375ba7c458131c52e
Brandon Worl <brandon.worl@gmail.com>
2014-10-29 19:27:07 -0700

    Removed dashboard controller and improved routing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:notice] = "Your account has been created."
-      redirect_to dashboard_url(@user)
+      redirect_to user_url(@user)
     else
       flash[:notice] = "There was a problem creating you."
       render :action => :new
@@ -19,11 +19,11 @@ class UsersController < ApplicationController
   end
 
   def show
-    if params.has_key?(:id) and current_user.has_role? :admin
-      @user = User.find(params[:id])
-    else
-      @user = current_user
-    end
+    @user = User.find(params[:id])
+  end
+
+  def dashboard
+    @user = current_user
   end
 
   def index
@@ -32,6 +32,7 @@ class UsersController < ApplicationController
   end
 
   def edit
+    return @user = current_user unless params.include? :id
     @user = User.find(params[:id])
   end
 
@@ -48,7 +49,7 @@ class UsersController < ApplicationController
 
     if @user.update_attributes(user_params)
       flash[:notice] = "Account updated!"
-      redirect_to @user
+      redirect_to user_url(@user)
     else
       render :action => :edit
     end
