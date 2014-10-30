d2e5c931a066a78b204da9e9f45c3b232172c6d6
Brandon Worl <naosyth@gmail.com>
2014-10-30 11:40:32 -0700

    Finished styling remaining pages

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -2,6 +2,15 @@ class UserSessionsController < ApplicationController
   before_filter :require_no_user, :only => [:new, :create]
   before_filter :require_user, :only => :destroy
 
+  def require_no_user
+    if current_user
+      store_location
+      flash[:notice] = "You must be logged out to access this page"
+      redirect_to dashboard_url
+      return false
+    end
+  end
+
   def new
     @user_session = UserSession.new
 
@@ -14,7 +23,7 @@ class UserSessionsController < ApplicationController
       flash[:notice] = "Login successful!"
       redirect_back_or_default dashboard_url(@current_user)
     else
-      render :action => :new
+      render :layout => "authentication", :action => :new
     end
   end
 
