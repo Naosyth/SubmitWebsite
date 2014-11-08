a4cc934b6300eeaa19d628b8751169473df6b269
Brandon Worl <brandon.worl@gmail.com>
2014-11-07 23:15:57 -0800

    Allowed instructors to create courses.

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,46 @@
 module CoursesHelper
+  def require_admin
+    if not current_user.has_role? :admin
+      flash[:notice] = "That action is only available to admins"
+      redirect_to dashboard_url
+    end
+  end
+
+  def require_instructor
+    return if current_user.has_role? :admin
+    
+    if not current_user.has_role? :instructor
+      flash[:notice] = "That action is only available to instructors"
+      redirect_to dashboard_url
+    end
+  end
+
+def require_instructor_owner
+    return if current_user.has_role? :admin
+
+    course = Course.find(params[:id])
+    if not current_user.has_role? :instructor, course
+      flash[:notice] = "That action is only available to the instructor of the course"
+      redirect_to dashboard_url
+    end
+  end
+
+  def require_student
+    return if current_user.has_role? :admin
+
+    if not current_user.has_role? :student
+      flash[:notice] = "That action is only available to students"
+      redirect_to dashboard_url
+    end
+  end
+
+  def require_student_enrolled
+    return if current_user.has_role? :admin
+
+    course = Course.find(params[:id])
+    if not current_user.has_local_role? :student, course
+      flash[:notice] = "That action is only available to students enrolled in the course"
+      redirect_to dashboard_url
+    end
+  end
 end
