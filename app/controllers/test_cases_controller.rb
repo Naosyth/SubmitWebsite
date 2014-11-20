41ff7c3e1df7fe36417f2a8fb781d4b75144e1d6
hthroweriii <hthroweriii@nevada.unr.edu>
2014-11-20 11:03:53 -0800

    Test Cases Controller

--- app/controllers/test_cases_controller.rb
+++ app/controllers/test_cases_controller.rb
@@ -1,11 +1,20 @@
 class TestCasesController < ApplicationController
 
-  # Creates a new test case
-  def create
-  end
-
   # Shows a test case
   def show
+    @test_case = TestCase.find(params[:id])
   end
-
+  
+  private
+    # Gets the course this test case belongs to
+    def get_course
+	    return TestCase.find(params[:id]).assignment.course
+    end
+    helper_method :get_course
+  
+    # Gets the course this test case belongs to
+    def get_assignment
+      return TestCase.find(params[:id]).assignment
+    end
+    helper_method :get_assignment
 end
