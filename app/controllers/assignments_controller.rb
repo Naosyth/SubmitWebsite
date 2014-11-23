6c82b28d27d310fcce32c9e9f9eae8adc7fec9cd
Brandon Worl <brandon.worl@gmail.com>
2014-11-22 16:30:15 -0800

    Made date time picker look better. Added validation of assignment creation.

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -16,11 +16,14 @@ class AssignmentsController < ApplicationController
     @course = Course.find(params[:course_id])
 
     convert_dates_to_utc
-    assignment = @course.assignments.create(assignment_params)
-    assignment.create_submissions_for_students
-    assignment.create_test_case
-
-    redirect_to course_path(@course)
+    @assignment = @course.assignments.new(assignment_params)
+    if @assignment.save
+      @assignment.create_submissions_for_students
+      @assignment.create_test_case
+      redirect_to course_path(@course)
+    else
+      render :action => :new
+    end
   end
 
   # Displays an assignment
@@ -44,12 +47,14 @@ class AssignmentsController < ApplicationController
   # Updates an assignment
   def update
     @assignment = Assignment.find(params[:id])
+    @course = @assignment.course
 
     convert_dates_to_utc
-
     if @assignment.update_attributes(assignment_params)
       flash[:notice] = "Assignment updated!"
       redirect_to course_path(@assignment.course)
+    else
+      render :action => :edit
     end
   end
 
@@ -62,12 +67,12 @@ class AssignmentsController < ApplicationController
   end
 
   private
-    def assignment_params
-      params.require(:assignment).permit(:name, :description, :start_date, :due_date, :lock)
-    end
+  def assignment_params
+    params.require(:assignment).permit(:name, :description, :start_date, :due_date, :lock)
+  end
 
-    def convert_dates_to_utc
-      params[:assignment][:start_date] = Time.at(params[:assignment][:start_date].to_i)
-      params[:assignment][:due_date] = Time.at(params[:assignment][:due_date].to_i)
-    end
+  def convert_dates_to_utc
+    params[:assignment][:start_date] = Time.at(params[:assignment][:start_date].to_i)
+    params[:assignment][:due_date] = Time.at(params[:assignment][:due_date].to_i)
+  end
 end
