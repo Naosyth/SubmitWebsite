172ed847863dc30c57b8d652b55400b37b8aa64f
Nolan Burfield <nburf@nevada.unr.edu>
2014-12-30 15:12:24 -0800

    Messing with a diif finder

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,5 +34,5 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.compile_directory = '/Users/nolanburfield/Documents/submitTest/tempDirectory/'
+  config.compile_directory = '/home/nolan/Documents/submitTest/tempDirectory/'
 end
