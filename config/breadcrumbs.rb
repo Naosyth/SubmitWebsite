38c3a528b3edc47e42d8ab09643e737be9bb193e
Brandon Worl <naosyth@gmail.com>
2015-02-19 13:43:46 -0800

    Started implementing breadcrumbs

---
+++ config/breadcrumbs.rb
@@ -0,0 +1,42 @@
+crumb :root do
+  link "Home", dashboard_path
+end
+
+crumb :courses do
+  link "Courses", courses_url
+end
+
+crumb :course do |course|
+  link course.name, course
+  parent :courses
+end
+
+crumb :course_users do |course|
+  link "Enrolled Users", courses_users_path(course)
+  parent :course, course
+end
+
+# crumb :projects do
+#   link "Projects", projects_path
+# end
+
+# crumb :project do |project|
+#   link project.name, project_path(project)
+#   parent :projects
+# end
+
+# crumb :project_issues do |project|
+#   link "Issues", project_issues_path(project)
+#   parent :project, project
+# end
+
+# crumb :issue do |issue|
+#   link issue.title, issue_path(issue)
+#   parent :project_issues, issue.project
+# end
+
+# If you want to split your breadcrumbs configuration over multiple files, you
+# can create a folder named `config/breadcrumbs` and put your configuration
+# files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
+# folder are loaded and reloaded automatically when you change them, just like
+# this file (`config/breadcrumbs.rb`).
